bca078f16c82dcd213f9377ec178621f9663f65c
Magelland0552 <richmagellan0552@gmail.com>
2026-02-16 10:43:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+ const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('HELLOW WORD');
-});
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
